838210d2ca85c484c1714ad2e006da3c3252069d
NaOH <2692246522@qq.com>
2025-01-26 21:19:07 +0000

    dataset butterflies

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ import os
 
 img_size = 512, 512
 count = 0
-for pic_path in os.listdir(r"C:\Users\26922\Desktop\flowers"):
-    pic = cv.imread(fr"C:\Users\26922\Desktop\flowers\{pic_path}")
+for pic_path in os.listdir(r"C:\Users\26922\Desktop\pic"):
+    pic = cv.imread(fr"C:\Users\26922\Desktop\pic\{pic_path}")
     x, y = pic.shape[0], pic.shape[1]
-    if max(x / y, y / x) + 0.12 > 1:
+    print(max(x / y, y / x))
+    if max(x / y, y / x) + 0.15 > 1:
         pic_ = cv.resize(pic[0 : min(x, y) - 1, 0 : min(x, y) - 1, :], img_size)
-        cv.imwrite(fr"./data/{count}.png", pic_)
+        cv.imwrite(fr"./dataset(butterflies)/{count}.png", pic_)
     count += 1
     if count >= 200:
         break
