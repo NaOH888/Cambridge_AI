7db9c21f2022ea6345b817e042d6d5af4a7e8e8f
NaOH <2692246522@qq.com>
2025-01-27 19:16:30 +0000

    111

--- src/AwakeNet.py
+++ src/AwakeNet.py
@@ -11,33 +11,33 @@ class AwakeNetGen(nn.Module):
     """
     def __init__(self):
         super(AwakeNetGen, self).__init__()
-        # in : [batch_size, 512, 512,
+        # in : [batch_size, 3, 512, 512]
         self.conv1 = nn.Conv2d(in_channels=net_params.input_image_depth,
                                out_channels=10,
                                kernel_size=(5,5),
                                stride=2,
                                padding=4)
-        self.pool1 = nn.AdaptiveAvgPool3d(output_size=(1024,1024,10))
+        self.pool1 = nn.MaxPool3d(kernel_size=5)
         self.activ1 = nn.LeakyReLU(negative_slope=0.2)
         self.conv2 = nn.Conv2d(in_channels=10,
                                kernel_size=(5,5),
                                stride=2,
                                padding=4)
-        self.pool2 = nn.AdaptiveAvgPool3d(output_size=(1024, 1024,10))
+        self.pool2 = nn.MaxPool3d(kernel_size=5)
         self.activ2 = nn.LeakyReLU(negative_slope=0.2)
         self.conv3 = nn.Conv2d(in_channels=10,
                                kernel_size=(5, 5),
                                stride=2,
                                padding=4)
-        self.pool3 = nn.AdaptiveAvgPool3d(output_size=(1024, 1024, 10))
+        self.pool3 = nn.AdaptiveAvgPool3d(output_size=(10,1024, 1024 ))
         self.activ3 = nn.LeakyReLU(negative_slope=0.2)
-        # 现在应该是 [batch, 1024, 1024, 10], 然后最后一维求和，变成[batch,2048, 5120]
+        # 现在应该是 [batch, 10, 1024, 1024], 然后第二维求和，变成[batch,2048, 5120]
         self.fc1 = nn.Linear(in_features=5120, out_features=1024,bias=True)
         self.activ4 = nn.LeakyReLU(negative_slope=0.2)
         # [batch,2048, 1024]
         self.fc2 = nn.Linear(in_features=1024, out_features=384, bias=True)
         self.activ5 = nn.Tanh()
-        # then resize to [batch, 512, 512, 3]
+        # then resize to [batch, 3, 512, 512]
 
 
     def forward(self, x : ndarray):
@@ -56,13 +56,13 @@ class AwakeNetGen(nn.Module):
         x = self.conv3(x)
         x = self.pool3(x)
         x = self.activ3(x)
-        x = torch.sum(x, dim=3, keepdim=False)
+        x = torch.sum(x, dim=1, keepdim=False)
         x = torch.reshape(x, shape=[net_params.batch_size,2048, 5120])
         x = self.fc1(x)
         x = self.activ4(x)
         x = self.fc2(x)
         x = self.activ5(x)
-        x = torch.reshape(x, shape=[net_params.batch_size, 512, 512, 3])
+        x = torch.reshape(x, shape=[net_params.batch_size, 3, 512, 512])
         return x
 
 class AwakeNetDis(nn.Module):
@@ -73,14 +73,14 @@ class AwakeNetDis(nn.Module):
                                kernel_size=(5,5),
                                stride=2,
                                padding=4)
-        self.pool1 = nn.AdaptiveAvgPool3d(output_size=(1024, 1024, 1))
+        self.pool1 = nn.AdaptiveAvgPool3d(output_size=(1, 1024, 1024))
         self.activ1 = nn.LeakyReLU(negative_slope=0.2)
         # reshape to (batch, 1024, 1024)
         self.fc1 = nn.Linear(in_features=1024, out_features=1024,bias=True)
         self.activ2 = nn.LeakyReLU(negative_slope=0.2)
         self.fc2 = nn.Linear(in_features=1024, out_features=1024, bias=True)
         self.activ3 = nn.LeakyReLU(negative_slope=0.2)
-        # squeeze
+        # flatten
         self.fc3 = nn.Linear(in_features=1024 * 1024, out_features=512, bias=True)
         self.activ4 = nn.LeakyReLU(negative_slope=0.2)
         self.fc4 = nn.Linear(in_features=512, out_features=1, bias=True)
@@ -89,7 +89,7 @@ class AwakeNetDis(nn.Module):
         if x.shape != [net_params.batch_size, net_params.input_image_depth, net_params.input_image_size]:
             raise ValueError(fr"Input's size should be "
                              fr"{net_params.batch_size, 
-                             net_params.input_image_depth, 
+                             net_params.input_image_depth,  
                              net_params.input_image_size}"
                              fr",but it is {x.shape}")
         x = self.conv1(x)
@@ -107,3 +107,26 @@ class AwakeNetDis(nn.Module):
         x = self.activ5(x)
         # out : [batch_size, 1]
         return x
+
+class AwakeNet(nn.Module):
+    def __init__(self, n_dis : int):
+        super(AwakeNet, self).__init__()
+        if n_dis < 1:
+            raise ValueError(fr"n_dis should be a positive integer")
+        self.generator = AwakeNetGen()
+        self.discriminators = nn.Sequential()
+        for i in range(n_dis):
+            self.discriminators.add_module(str(i), AwakeNetDis())
+    def forward(self, x : torch.Tensor, dream : torch.Tensor):
+        if x.shape != [net_params.batch_size, net_params.input_image_depth, net_params.input_image_size]:
+            raise ValueError(fr"Input's size should be "
+                             fr"{net_params.batch_size, 
+                             net_params.input_image_depth,  
+                             net_params.input_image_size}"
+                             fr",but it is {x.shape}")
+        if x.shape != dream.shape:
+            raise ValueError(fr"The shape of raw image and dreamt image should be the same")
+        '''
+        HOW TO WRITE!!!
+        
+        '''
