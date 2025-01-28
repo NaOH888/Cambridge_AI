0a701d096f3d093b61036c76841961d63b00f7c6
NaOH <2692246522@qq.com>
2025-01-28 16:06:36 +0000

    initial gan model

--- src/trainer.py
+++ src/trainer.py
@@ -2,6 +2,8 @@ import math
 import os
 
 import torch.nn as nn
+from torch import dtype
+
 import AwakeNet
 import torch
 import net_params
@@ -39,11 +41,35 @@ def train(net : AwakeNet, real_imgs : torch.Tensor, fake_dreamt_img : torch.Tens
         if whole_size <= net_params.batch_size:
             train_one_batch(net, real_imgs, fake_dreamt_img)
         else:
-            for i in range(math.ceil(whole_size / net_params.batch_size)):
+            for j in range(math.ceil(whole_size / net_params.batch_size)):
                 train_one_batch(
                     net,
-                    real_imgs[i * net_params.batch_size: min((i + 1) * net_params.batch_size - 1, whole_size)],
-                    fake_dreamt_img[i * net_params.batch_size: min((i + 1) * net_params.batch_size - 1, whole_size)])
+                    real_imgs[j * net_params.batch_size: min((j + 1) * net_params.batch_size - 1, whole_size)],
+                    fake_dreamt_img[j * net_params.batch_size: min((j + 1) * net_params.batch_size - 1, whole_size)])
+
+def pretrain_for_generator(net : AwakeNet, real_imgs, dream_imgs, epoches):
+    gen = net.generator
+    whole_size = real_imgs.shape[0]
+    t_criterion = nn.MSELoss()
+    for epoch in range(epoches):
+        net.saveParam()
+        print("model saved")
+        print(f"epoch {epoch + 1} start training")
+        total_loss = 0
+        blocks = math.ceil(whole_size / net_params.batch_size)
+        for i in range(blocks):
+
+            t_r_i = real_imgs[i * net_params.batch_size: min((i + 1) * net_params.batch_size - 1, whole_size)]
+            t_d_i = dream_imgs[i * net_params.batch_size: min((i + 1) * net_params.batch_size - 1, whole_size)]
+            gen.optim.zero_grad()
+            generated = gen(t_d_i)
+            loss = t_criterion(generated, t_r_i)
+            total_loss += loss.item()
+            #print(f"    batch {i + 1} loss : {loss.item()}")
+            loss.backward()
+            gen.optim.step()
+        print(f"epoch {epoch + 1} loss : {total_loss / blocks}")
+
 
 ## test
 print(device)
@@ -64,18 +90,15 @@ r_fold = os.listdir(r"./raw_data")
 for r in r_fold:
     di = cv.resize(cv.imread(r"./dream_data/" + r), [net_params.input_image_size_x, net_params.input_image_size_y])
     ri = cv.resize(cv.imread(r"./raw_data/" + r), [net_params.input_image_size_x, net_params.input_image_size_y])
-    dream_img.append(torch.tensor(di \
-                                  .reshape((net_params.input_image_depth,
-                                           net_params.input_image_size_x,
-                                           net_params.input_image_size_y)),dtype=torch.float32))
-    real_img.append(torch.tensor(ri\
-                                 .reshape((net_params.input_image_depth,
-                                           net_params.input_image_size_x,
-                                           net_params.input_image_size_y)),dtype=torch.float32))
+    di = torch.transpose(torch.transpose(torch.tensor(di,dtype=torch.float32), 0, 2),1,2)
+    ri = torch.transpose(torch.transpose(torch.tensor(ri, dtype=torch.float32), 0, 2),1,2)
+    dream_img.append(di)
+    real_img.append(ri)
 
-r_datas = torch.stack(real_img,dim=0)
-f_datas = torch.stack(dream_img, dim=0)
+r_datas = torch.stack(real_img,dim=0).to(device)
+f_datas = torch.stack(dream_img, dim=0).to(device)
 print("end loading data")
 print("start training")
-train(train_net, r_datas.to(device), f_datas.to(device))
+#train(train_net, r_datas, f_datas)
 
+pretrain_for_generator(train_net, r_datas, f_datas, epoches=100)
